65748a98ceab7f81a4718bb9a3ee17cb0de31b75
Oleksandr Prokopets <prokopets.alexandr@gmail.com>
2021-12-05 09:31:24 +0200

    Fix log off cart issue

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -8,18 +8,16 @@ namespace TRMDesktopUI.ViewModels
 {
     public class ShellViewModel : Conductor<object>, IHandle<LogOnEvent>
     {
-        private SalesViewModel _salesVM;
         private IEventAggregator _eventAggregator;
         private ILoggedInUserModel _user;
 
-        public ShellViewModel(IEventAggregator eventAggregator, SalesViewModel salesVM, ILoggedInUserModel user)
+        public ShellViewModel(IEventAggregator eventAggregator, ILoggedInUserModel user)
         {
-            _salesVM = salesVM;
+          
             _user = user;
-
             _eventAggregator = eventAggregator;
-            _eventAggregator.SubscribeOnPublishedThread(this);
 
+            _eventAggregator.SubscribeOnPublishedThread(this);
             ActivateItemAsync(IoC.Get<LoginViewModel>());
         }
 
@@ -42,7 +40,7 @@ namespace TRMDesktopUI.ViewModels
 
         public async Task HandleAsync(LogOnEvent message, CancellationToken cancellationToken)
         {
-            await ActivateItemAsync(_salesVM);
+            await ActivateItemAsync(IoC.Get<SalesViewModel>());
             NotifyOfPropertyChange(() => IsLoggedIn);
         }
 
